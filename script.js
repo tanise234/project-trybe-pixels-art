127673b84485611c2fba2739a3eeb8963d631ab4
Tanise Santos <tanise234@gmail.com>
2022-03-23 07:50:40 -0300

    requisitos 7 e 8 concluídos

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+// requisito 6 - iniciar com a classe selected na cor1
+let corSelecionada = document.querySelector('.cor1');
+corSelecionada.classList.add('selected');
+
+// adicionar eventListener na paleta de cores
+let cores = document.querySelectorAll('.color');
+for (cor of cores) {
+  cor.addEventListener('click', selecionaCor);
+}
+
+let pix;
 function createPixel(altura, largura) {
   for (let i = 0; i < altura; i += 1) {
     for (let j = 0; j < largura; j += 1) {
-      const pix = document.createElement('div');
+      pix = document.createElement('div');
       pix.classList = 'pixel';
       pix.backgroundColor = 'white';
-      pix.addEventListener('click', function () {
-        pix.style.backgroundColor = 'pink';
-      });
+      pix.addEventListener('click', aplicaCor);
       document.getElementById('pixel-board').appendChild(pix);
-
     }
     const pulaLinha = document.createElement('br');
     document.getElementById('pixel-board').appendChild(pulaLinha);
   }
 }
 createPixel(5, 5);
+
+// selecionar cor da paleta
+function selecionaCor(event) {
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+// aplicar cor no pixel-board
+function aplicaCor(event) {
+  let classeDaCor = document.querySelector('.selected').classList;
+  event.target.classList = classeDaCor;
+}
