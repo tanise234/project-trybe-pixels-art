d546fdea2e33d2ee34d4fdfe5155680fe5f07eb8
Tanise Santos <tanise234@gmail.com>
2022-03-29 21:23:07 -0300

    projeto finalizado!!!

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function escolheCor(novaCor) {
+  let r = Math.ceil(Math.random() * 255);
+  let g = Math.ceil(Math.random() * 255);
+  let b = Math.ceil(Math.random() * 255);
+  novaCor.style.backgroundColor =
+  'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+// criar paleta de cores
+function coresNovas(){
+  let cor1 = document.createElement('li');
+    cor1.classList.add('color','cor1', 'selected');
+    cor1.style.backgroundColor = 'black';
+    document.getElementById('color-palette').appendChild(cor1);
+  for (i =0; i < 3; i += 1){
+    let novaCore = document.createElement('li');
+    escolheCor(novaCore);
+    novaCore.classList.add('color');
+    novaCore.classList.add('cor'+(i+2));
+    document.getElementById('color-palette').appendChild(novaCore);
+  }
+}
+
+coresNovas();
+
 // adicionar eventListener na paleta de cores
 let cores = document.querySelectorAll('.color');
 for (let i = 0; i < cores.length; i += 1) {
@@ -28,17 +53,15 @@ createPixel(5, 5);
 
 // aplicar cor no pixel-board
 function aplicaCor(event) {
-  let classeDaCor = document.querySelector('.selected').classList;
-  event.target.classList = classeDaCor;
-  event.target.classList.remove('selected');
-  event.target.classList.add('pixel');
+  let corEscolhida = document.querySelector('.selected');
+  event.target.style.backgroundColor = corEscolhida.style.backgroundColor;
 }
 
 // botão limpar
 function limpaQuadroDePixels() {
   let pixels = document.querySelectorAll('section div');
-  for (p in pixels) {
-    pixels[p].classList = 'pixel';
+  for (i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
   }
 }
 
@@ -48,7 +71,7 @@ botaoLimpar.addEventListener('click', limpaQuadroDePixels);
 // botão mudar tamanho do quadro
 function mudarTamanho() {
   let n = document.getElementById('board-size').value;
-  if (n == false){
+  if (n == false) {
     alert('Board inválido!');
   }
   let pixels = document.querySelectorAll('section div');
